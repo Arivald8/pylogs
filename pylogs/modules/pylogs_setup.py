38e7a4b6f517ae4001f8984ec2110c76134edad5
Arivald8 <arivald88@gmail.com>
2022-07-12 16:16:24 +0100

    Added first unit test for database creation.

--- pylogs/modules/pylogs_setup.py
+++ pylogs/modules/pylogs_setup.py
@@ -5,6 +5,7 @@ class PylogsSetup:
     def __init__(
         self,
         db_name="pylogs.db",
+        db_path="pylogs/",
         db_users_table="users",
         db_events_table="events",
         user="user",
@@ -12,6 +13,7 @@ class PylogsSetup:
 
     ):
         self.db_name = db_name
+        self.db_path = db_path
         self.db_users_table = db_users_table
         self.db_events_table = db_events_table
         self.user = user
@@ -19,12 +21,12 @@ class PylogsSetup:
 
     
     def check_db_exists(self) -> bool:
-        return True if Path(f"pylogs/{self.db_name}").is_file() else False
+        return True if Path(f"{self.db_path}{self.db_name}").is_file() else False
 
 
     def create_db(self) -> bool:
         try:
-            open(f"pylogs/{self.db_name}", 'x')
+            open(f"pylogs/{self.db_name}", 'x').close()
             return True
         except:
             return False
