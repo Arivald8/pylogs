afc96734d7ec202d106e5d3fd06fc9f1511dd064
Arivald8 <arivald88@gmail.com>
2022-07-13 14:02:53 +0100

    Refactored the exception handler inside of db_processor.py

--- pylogs/modules/processors/db_processor.py
+++ pylogs/modules/processors/db_processor.py
@@ -1,8 +1,10 @@
 import sqlite3
 
 from modules.pylogs_setup import PylogsSetup
+from processors.exception_processor import ExceptionProcessor
 
 setup_process = PylogsSetup()
+exception_process = ExceptionProcessor()
 
 class DbProcessor:
     def __init__(self, process_user: tuple):
@@ -13,4 +15,4 @@ class DbProcessor:
         try:
             return sqlite3.connect(f"{setup_process.db_path}{setup_process.db_name}")
         except Exception as db_connect_error:
-            log_error(db_connect_error)
+            exception_process.log_error(db_connect_error)
